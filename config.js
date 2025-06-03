1e41bb365ee06f0a1a558aebb7f0aadb9a99c5e1
TroyDGamer <troyarthur99@gmail.com>
2025-06-03 22:39:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 
 global.owner = process.env.OWNER_NUMBER; 
 global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
-global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
-global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
+global.ownername = process.env.OWNER_NAME || "么・TRØY99 🎧🤍";
+global.packname = process.env.PACK_NAME || "Trøy・🤪";
+global.author = process.env.AUTHOR || "";
+global.channelname = process.env.CHANNEL_NAME || "";
 global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
 global.autoTyping = process.env.AUTO_TYPING === 'true'; 
 global.autoRecord = process.env.AUTO_RECORD === 'true';
 global.autoViewStatus = process.env.AUTO_VIEW_STATUS === 'true';
 global.AUTOSTATUS_REACT = process.env.AUTOSTATUS_REACT === 'true';
-global.autoLikeEmoji = process.env.AUTO_LIKE_EMOJI || "💚";
+global.autoLikeEmoji = process.env.AUTO_LIKE_EMOJI || "🫡";
 
 global.LEVELUP = process.env.LEVELUP === 'true';
 
